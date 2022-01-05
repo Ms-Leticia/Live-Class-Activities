e4f4177eb8beb87f615623118953ec57bd0d1e05
ms.leticia <ms.leticia@outlook.com>
2022-01-04 21:42:45 -0500

    This is a commit

---
+++ 03-JAVA_SCRIPT/03-JavaScript/01-Activities/02-Iteration/Unsolved/script.js
@@ -0,0 +1,14 @@
+// TODO: Write Your JavaScript Code Here
+var students = ['Sierra','Leticia','Michael','Ernest','Rodrigo']
+
+console.log(students.length);
+
+for (var i=0; i<students.length; i++) {
+    console.log('Great to See you,'+students [i]+'!'); 
+
+while (condition) {
+    
+}
+}
+
+
