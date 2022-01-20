266cc6cefd515a62b8549eb6726011a71872cd0a
Ms-Leticia <ms.leticia@outlook.com>
2022-01-19 20:39:14 -0500

    added Autocomplete widget

--- SCRATCH NOTES/script.js
+++ SCRATCH NOTES/script.js
@@ -22,6 +22,34 @@ var handleFormSubmit = function (event) {
     return;
   }
 
+  // Autocomplete widget
+  $(function () {
+    var skillNames = [
+      'Bootstrap',
+      'C',
+      'C++',
+      'CSS',
+      'Express.js',
+      'Git',
+      'HTML',
+      'Java',
+      'JavaScript',
+      'jQuery',
+      'JSON',
+      'MySQL',
+      'Node.js',
+      'NoSQL',
+      'PHP',
+      'Python',
+      'React',
+      'Ruby',
+    ];
+    $('#skill-name').autocomplete({
+      source: skillNames,
+    });
+  });
+  
+
   printSkills(nameInput, dateInput);
 
   nameInputEl.val('');
